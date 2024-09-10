ba4afdc7ba98a96b91508a18415a315564b161ca
Oziel Sousa <ozielzin358@gmail.com>
2024-09-09 22:46:10 -0300

    feat: adds pandas lib and data base

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+#Importar DB
+
+#Ver base de dados
+
+#Faturamento por Loja
+
+#Quantidade de produtos vendidos por loja
+
+#Ticket medio por produto em cada loja
+
+#Enviar um relatório por e-mail
